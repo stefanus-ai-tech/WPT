f706dbb79d7ee84de7827f2ba3f68750cc1eb85d
Stefanus Adri Irawan <stefanusaitech@gmail.com>
2025-02-07 07:17:32 +0700

    edited a bit

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ import sqlite3
 import signal
 import sys
 
-test_mode_model_quick = False  # Set to True for quick testing, False for full model
+test_mode_model_quick = True  # Set to True for quick testing, False for full model
 
 if test_mode_model_quick:
     llm_model = "llama-3.1-8b-instant"
@@ -44,6 +44,8 @@ CORS(
                 "http://localhost:3000",
                 "http://localhost:5174",
                 "http://127.0.0.1:5174",
+                "http://localhost:5173",
+                "http://127.0.0.1:5173"
             ],
             "methods": ["GET", "POST", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"],
